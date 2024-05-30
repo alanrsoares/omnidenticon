33340067d57ae34157ed979f96c0b939a51aa751
alanrsoares <alan@axelar.network>
2024-05-30 13:27:19 +1200

    feat: lit element

---
+++ packages/lit/src/omnidenticon.ts
@@ -0,0 +1,52 @@
+import { LitElement, html } from "lit";
+import { createRef, ref } from "lit/directives/ref.js";
+import { customElement, property } from "lit/decorators.js";
+
+import { DEFAULT_COLORS, OmnidenticonGenerator } from "@omnidenticon/core";
+import { addressToNumber } from "@omnidenticon/core/utils";
+
+@customElement("omni-identicon")
+export class Omnidenticon extends LitElement {
+  @property()
+  address = "";
+
+  @property({ type: String })
+  prefix = "";
+
+  @property({ type: Number })
+  diameter = 100;
+
+  @property()
+  ref = createRef();
+
+  get hasPrefix() {
+    return Boolean(this.prefix);
+  }
+
+  get seed() {
+    return addressToNumber(this.address, this.prefix);
+  }
+
+  firstUpdated(): void {
+    const generator = new OmnidenticonGenerator(
+      this.seed,
+      this.diameter,
+      5,
+      DEFAULT_COLORS
+    );
+
+    const identiconContainer = generator.generateIdenticon();
+
+    this.ref.value?.replaceWith(identiconContainer);
+  }
+
+  render() {
+    return html` <div ${ref(this.ref)}></div> `;
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    "my-element": Omnidenticon;
+  }
+}
